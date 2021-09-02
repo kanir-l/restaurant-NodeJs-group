e8cf91eaf343b1f7e1f4e60d2b9ff74c024ea3dc
kanir-l <kr@kanir.se>
2021-09-02 10:42:22 +0200

    created post new reservation from the frontend

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.post("/reservations/checkingEightteen", eightteenChecking);
 router.post("/reservations/checkingTwentyone", twentyoneChecking);
-router.get("/reservations/confirmation", createReservations);
+router.post("/reservations/confirmation", createReservations);
 
 module.exports = router;
