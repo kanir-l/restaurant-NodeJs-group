a19c87a1f3e34bae8e2bd5bf5135651c257bf43e
Lovisa Bohman <lovisabo@hotmail.com>
2021-09-09 00:37:58 +0200

    Create a route for the cancel page and a delete route.

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const {sendingAvailability, createReservations } = require("../controller/reservationController");
+const {sendingAvailability, createReservations, deleteReservation } = require("../controller/reservationController");
 const router = express.Router();
 
 router.get("/reservations/checkingAvailability", sendingAvailability)
 router.post("/reservations/confirmation", createReservations);
 router.get("/reservations/confirmation", createReservations);
+router.get("/reservations/delete/:id", deleteReservation);
+router.delete("/reservations/delete/:id", deleteReservation);
 
 module.exports = router;
