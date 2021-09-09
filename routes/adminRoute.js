1c0f1c6e520d9af300cc6a49634c529c3c6b828d
Lovisa Bohman <lovisabo@hotmail.com>
2021-09-09 13:43:40 +0200

    Added comments to all the routes.

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,10 +2,10 @@ const express = require("express");
 const { renderBookingsAdmin, deleteBookingAdmin, updateBookingAdmin, sendingAvailabilityEdit, renderBooking} = require("../controller/adminController");
 const router = express.Router();
 
-router.get("/admin", renderBookingsAdmin);
-router.delete("/admin/delete/:id", deleteBookingAdmin); 
-router.get("/admin/checkingAvailabilityEdit", sendingAvailabilityEdit);
-router.put("/admin/update", updateBookingAdmin);
-router.get("/admin/:id", renderBooking) // This route is when the user goes to do their own cancellation
+router.get("/admin", renderBookingsAdmin);// This route handles fetching all the reservations from the db
+router.delete("/admin/delete/:id", deleteBookingAdmin); // This route handles the deletion of the reservation from admin page
+router.get("/admin/checkingAvailabilityEdit", sendingAvailabilityEdit); // This route handles the checks for editing the reservation
+router.put("/admin/update", updateBookingAdmin); // This route handles the edit feature in admin page
+router.get("/admin/:id", renderBooking); // This route is when the user goes to do their own cancellation
 
 module.exports = router;
