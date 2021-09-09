c21f975cfa8547558c239fcd0602c760f6f99f9b
Lovisa Bohman <lovisabo@hotmail.com>
2021-09-09 10:34:28 +0200

    Remove the routes from reservationController to use Kanistha's in the Admin for cancel-link.

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/reservations/checkingAvailability", sendingAvailability)
 router.post("/reservations/confirmation", createReservations);
 router.get("/reservations/confirmation", createReservations);
-router.get("/reservations/delete/:id", deleteReservation);
-router.delete("/reservations/delete/:id", deleteReservation);
+// router.get("/reservations/delete/:id", deleteReservation);
+// router.delete("/reservations/delete/:id", deleteReservation);
 
 module.exports = router;
